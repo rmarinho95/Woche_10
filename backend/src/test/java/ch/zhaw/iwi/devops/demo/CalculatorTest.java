4410ca8094cee1b149c460ba29028db6d3591dd0
rmarinho95 <58737804+rmarinho95@users.noreply.github.com>
2023-05-25 20:02:24 +0200

    Codeerweiterung CalculatorTest.java

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/CalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/CalculatorTest.java
@@ -1,30 +1,40 @@
 package ch.zhaw.iwi.devops.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void testAdd() {
-        Calculator calculator = new Calculator();
         assertEquals(4, calculator.add(2, 2));
     }
 
     @Test
     public void testSubtract() {
-        Calculator calculator = new Calculator();
         assertEquals(0, calculator.subtract(2, 2));
     }
 
     @Test
     public void testMultiply() {
-        Calculator calculator = new Calculator();
         assertEquals(4, calculator.multiply(2, 2));
     }
 
     @Test
     public void testDivide() {
-        Calculator calculator = new Calculator();
         assertEquals(2, calculator.divide(4, 2));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(4, 0));
     }
 }
